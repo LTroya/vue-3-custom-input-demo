a05b34c9c1fc6a763f79317b404559ecbdb79237
LuisTroya <luis.troya@allansolutions.com>
2023-07-18 22:23:14 -0500

    add demo

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import CustomInput from './components/CustomInput.vue'
+import {reactive} from "vue";
+
+const form = reactive({
+  name: ''
+})
+</script>
+
+<template>
+  <CustomInput v-model="form.name" placeholder="Placeholder sent by props"/>
+
+  {{ form }}
+</template>
---
+++ src/components/CustomInput.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(['modelValue', 'placeholder'])
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      :placeholder="placeholder"
+  />
+</template>
